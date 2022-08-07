5967d49a89636787f6bf4d613e757cb4b6153335
Beloved <1425279634@qq.com>
2022-08-07 22:12:36 +0800

    包结构变更

---
+++ bill-system/src/main/java/com/beloved/system/utils/SecurityUtils.java
@@ -0,0 +1,45 @@
+package com.beloved.system.utils;
+
+import com.beloved.common.enums.ErrorCode;
+import com.beloved.common.exception.ServiceException;
+import com.beloved.system.security.bo.LoginUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * SpringSecurity 工具类
+ *
+ * @author beloved
+ */
+public class SecurityUtils {
+
+    /**
+     * 获取用户id
+     */
+    public static Long getUserId() {
+        try {
+            return getLoginUser().getUser().getUserId();
+        } catch (Exception e) {
+            throw new ServiceException(ErrorCode.GET_USER_FAIL);
+        }
+    }
+    
+    /**
+     * 获取用户信息
+     */
+    public static LoginUser getLoginUser() {
+        try {
+            return (LoginUser) getAuthentication().getPrincipal();
+        } catch (Exception e) {
+            throw new ServiceException(ErrorCode.GET_USER_FAIL);
+        }
+    }
+    
+    /**
+     * 获取Authentication
+     */
+    public static Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+}
